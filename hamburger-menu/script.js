73e82bf702cc96bf899f1a01f8b4427d78666de2
unknown <Dustin.Guillen14@yahoo.com>
2020-12-19 00:29:19 -0700

    ADDED second hamburger menu on right side/ tweaked styling

--- hamburger-menu/script.js
+++ hamburger-menu/script.js
@@ -1,7 +1,17 @@
-const button = document.getElementById('button');
-const navbar = document.getElementById('nav');
+const buttonLeft = document.getElementById('button-left');
+const navbarLeft = document.getElementById('nav-left');
 
-button.addEventListener('click', function(){
-    navbar.classList.toggle("active");
-    button.classList.toggle("active");
+buttonLeft.addEventListener('click', function(){
+    navbarLeft.classList.toggle("active");
+    buttonLeft.classList.toggle("active");
+})
+
+
+
+const buttonRight = document.getElementById('button-right');
+const navbarRight = document.getElementById('nav-right');
+
+buttonRight.addEventListener('click', function(){
+    navbarRight.classList.toggle("active");
+    buttonRight.classList.toggle("active");
 })
