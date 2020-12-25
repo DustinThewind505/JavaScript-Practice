2a17df785ef070a09e749d4d308b9543e85728d6
unknown <Dustin.Guillen14@yahoo.com>
2020-12-25 15:03:29 -0700

    FUNCTIONALITY added random color buttons

--- background-change/script.js
+++ background-change/script.js
@@ -1,6 +1,21 @@
 const green = document.getElementById("green");
 const yellow = document.getElementById("yellow");
 const blue = document.getElementById("blue");
+const random1 = document.getElementById("random1");
+const random2 = document.getElementById("random2");
+
+const header = document.querySelector("h1");
+
+const setColors = [
+    "green",
+    "yellow",
+    "blue",
+    "purple",
+    "red",
+    "white",
+    "brown",
+    "grey"
+]
 
 green.addEventListener("click", () => {
     document.body.style.backgroundColor = "green"
@@ -13,3 +28,21 @@ yellow.addEventListener("click", () => {
 blue.addEventListener("click", () => {
     document.body.style.backgroundColor = "blue"
 })
+
+random1.addEventListener("click", () => {
+    document.body.style.backgroundColor = randomColors(setColors)
+})
+
+random2.addEventListener("click", () => {
+    document.body.style.backgroundColor = randomBg()
+})
+
+
+
+function randomColors(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function randomBg() {
+    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+}
